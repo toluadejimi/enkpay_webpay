28f0a5e6a0aa923b40e0f80ec466dcee7d6622b4
Jimmy <toluadejimi@gmail.com>
2023-05-24 19:50:18 +0100

    lloo

--- resources/views/webpay.blade.php
+++ resources/views/webpay.blade.php
@@ -402,7 +402,7 @@
 
                                                         <div class="col-md-6 mt-5">
                                                             <a href="decline?trans_id={{ $trans_id }}"
-                                                                class="btn btn-danger placeicon">
+                                                                class="btn btn-danger placeicon"></a>
                                                         </div>
 
 

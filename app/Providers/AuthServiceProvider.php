976d1a9ead7a213e1e76e65545b1405047aa5437
Jimmy <toluadejimi@gmail.com>
2023-12-28 20:04:03 +0100

    pushhh

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 // use Illuminate\Support\Facades\Gate;
+use Carbon\Carbon;
+use Laravel\Passport\Passport;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
@@ -21,6 +23,10 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        $this->registerPolicies();
+
+        Passport::tokensExpireIn(Carbon::now()->addMinutes(100));
+        Passport::personalAccessTokensExpireIn(Carbon::now()->addMinutes(100));
+        Passport::refreshTokensExpireIn(Carbon::now()->addMinutes(100));
     }
 }

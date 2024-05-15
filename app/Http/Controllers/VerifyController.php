ca93588c62b3a25fa5e37f1e25e73cab29d8ea05
Jimmy <toluadejimi@gmail.com>
2024-05-15 15:49:04 +0100

    adjusted

--- app/Http/Controllers/VerifyController.php
+++ app/Http/Controllers/VerifyController.php
@@ -71,7 +71,7 @@ class VerifyController extends Controller
         if ($user->role == 9 && $user->status == 2)
             $credentials = $request->only('email', 'password');
         if (Auth::attempt($credentials)) {
-            return view('payment'); // Redirect to dashboard or any other authenticated page
+            return redirect('payment'); // Redirect to dashboard or any other authenticated page
         }
 
         return back()->with('error', 'Email or password incorrect');

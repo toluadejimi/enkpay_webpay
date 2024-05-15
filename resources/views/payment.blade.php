163a5856540bf9aac6f825eaf46dc285095c91f0
Jimmy <toluadejimi@gmail.com>
2024-05-15 18:12:26 +0100

    adjusted

--- resources/views/payment.blade.php
+++ resources/views/payment.blade.php
@@ -4,6 +4,8 @@
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta http-equiv="refresh" content="20">
+
     <!-- Mobile Specific Metas -->
     <meta name="viewport"
           content="width=device-width, initial-scale=1, maximum-scale=1, minimum-scale=1, viewport-fit=cover">

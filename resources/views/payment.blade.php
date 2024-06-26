f5ce94f4a46ff7307ae24112ab5e972f12b5dbfe
Jimmy <toluadejimi@gmail.com>
2024-06-26 20:09:25 +0100

    adjusted

--- resources/views/payment.blade.php
+++ resources/views/payment.blade.php
@@ -358,6 +358,7 @@
                                             Vera
                                         </a>
                                     @endif
+                                @endif
 
 
                             </div>

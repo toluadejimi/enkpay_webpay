c6271d89969d0a9066f90f670ac8ea5cbc9e71cc
Jimmy <toluadejimi@gmail.com>
2024-07-28 07:28:26 +0100

    adjustedssdd

--- app/Http/Controllers/DataController.php
+++ app/Http/Controllers/DataController.php
@@ -176,11 +176,16 @@ class DataController extends Controller
             ));
 
             $var = curl_exec($curl);
+
+            dd($var);
+
             curl_close($curl);
 
             $var = json_decode($var);
 
-            dd($var);
+
+
+
             $trx_id = $var->requestId ?? null;
             $get_message = $var->response_description ?? null;
             $message = "Error Message from VAS DATA BUNDLE - $get_message";

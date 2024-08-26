c1342621c51b9f9e9fe58f37defd0a07b357f420
Jimmy <toluadejimi@gmail.com>
2024-08-26 22:36:34 +0100

    adjustedssdd

--- app/Http/Controllers/VerifyController.php
+++ app/Http/Controllers/VerifyController.php
@@ -1147,6 +1147,7 @@ class VerifyController extends Controller
 
         }
 
+        dd($ckstatus);
 
         return back()->with('error', 'Account number you provided is not correct, please check and try again');
 

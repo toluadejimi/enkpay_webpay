2c5ccb82074ecbf3040b9b2bcae83577a466a179
Jimmy <toluadejimi@gmail.com>
2024-08-26 23:12:56 +0100

    adjustedssdd

--- app/helpers.php
+++ app/helpers.php
@@ -2226,7 +2226,7 @@ if (!function_exists('credit_user_wallet')) {
 
         $var = curl_exec($curl);
 
-        //dd($var, $url, $user_email, $amount, $order_id);
+        dd($var, $url, $user_email, $amount, $order_id);
 
 
         curl_close($curl);

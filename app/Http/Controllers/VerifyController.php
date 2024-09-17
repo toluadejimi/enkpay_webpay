69e3a37c141e4585366d7463dbbaf72f5074874b
Jimmy <toluadejimi@gmail.com>
2024-09-17 09:12:10 +0100

    adjustedssdd

--- app/Http/Controllers/VerifyController.php
+++ app/Http/Controllers/VerifyController.php
@@ -830,6 +830,7 @@ class VerifyController extends Controller
             return back()->with('error', 'Please note that your payment failed, Your reversal has been processed back to your bank account ');
         }
 
+        dd($ckstatus);
 
 
         if ($ckstatus == "2") {

588f185300ef3391388f5db77001355db1ece8a9
Jimmy <toluadejimi@gmail.com>
2024-09-18 13:42:14 +0100

    adjustedssdd

--- app/helpers.php
+++ app/helpers.php
@@ -2171,6 +2171,17 @@ if (!function_exists('verify_payment_woven')) {
         $pstatus = $var->data->transactions[0]->status ?? null;
         $acct_no = $var->data->transactions[0]->nuban ?? null;
 
+        dd($var);
+
+
+        if ($status == "success" && $pstatus == "ACTIVE" && $ref == $acct_no) {
+            $data['amount'] = $var->data->transactions[0]->amount;
+            return $data;
+        }elseif($status == "success" && $pstatus == "ACTIVE" && $ref == $acct_no){
+            $data['message'] = "Transaction Not found";
+            return $data;
+        }
+
 
         if ($status == "success" && $pstatus == "ACTIVE" && $ref == $acct_no) {
             $data['amount'] = $var->data->transactions[0]->amount;

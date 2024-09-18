b4cc6b83e24be9e066596593e75925579653614e
Jimmy <toluadejimi@gmail.com>
2024-09-18 09:44:33 +0100

    adjustedssdd

--- resources/views/webpaywoven.blade.php
+++ resources/views/webpaywoven.blade.php
@@ -264,6 +264,14 @@
                 </svg>
 
 
+                @php
+                    if($payable_amount > 15000){
+                        $f_amount = $payable_amount + $psb_cap;
+                    }else{
+                      $f_amount = $payable_amount + $psb_charge;
+                    }
+
+                @endphp
 
 
                 <h1>₦{{ number_format($f_amount, 2) }}</h1>

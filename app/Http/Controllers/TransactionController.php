bb66fa86d5d40a692a860b9cef2eec61b43fb7df
Jimmy <toluadejimi@gmail.com>
2024-12-04 00:19:15 +0100

    adjustedssdd

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -1707,9 +1707,8 @@ class TransactionController extends Controller
             ->first()->amount ?? 0;
 
         if($get_amount == 0){
-           $amount =  Transfertransaction::where('ref', $request->pref)->first()->amount;
+           $amount =  Transfertransaction::where('ref', $trans_id)->first()->amount;
         }else{
-
             $amount = $get_amount;
         }
 

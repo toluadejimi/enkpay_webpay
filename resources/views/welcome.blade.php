7b24c5c1df47c94f624dcfb1cd4b68a0e880ffc0
Jimmy <toluadejimi@gmail.com>
2024-12-07 15:46:18 +0100

    adjustedssdd

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -17,6 +17,11 @@
         </style>
     </head>
     <body>
+
+    <div class='MainAdverTiseMentDiv' data-publisher="eyJpdiI6IlIvS2NJRlpiYkozNEIzUG1TREY3RWc9PSIsInZhbHVlIjoidkMyWmJXTytHenZOMWdCOWpnbVgzQT09IiwibWFjIjoiOGVjNzQwZjJkOTJiYjhhMWE5NjhkMjA5NDc1ZjdiYzU4ZmM5MzFkNzFhZTg2N2MwNzc2M2UwMDU1MmNmNmYzNCIsInRhZyI6IiJ9" data-adsize="735x372">
+
+    </div>
+    <script class="adScriptClass" src="https://advert.tomitechltd.com/assets/ads/ad.js"></script>
     </body>
 
     <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/botman-web-widget@0/build/assets/css/chat.min.css">
